09c62c07965578479543fe797a24066c5bfd09f9
Navid Rida <navid.anzum88@gmail.com>
2019-11-14 17:00:05 +0600

    First commit project initialization

---
+++ erp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required,user_passes_test
+
+# Create your views here.
+@login_required
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
