f9f3f21f7ae4ba12383929d17bffe9fec19ad719
Navid Rida <navid.anzum88@gmail.com>
2019-11-19 18:27:44 +0600

    static files and login

--- erp/views.py
+++ erp/views.py
@@ -5,4 +5,6 @@ from django.contrib.auth.decorators import login_required,user_passes_test
 # Create your views here.
 @login_required
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    context = {'testdata': "This is just for testing"}
+    return render(request, 'erp/blank-page-header.html', context)
--- rubik/settings.py
+++ rubik/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'erp.apps.ErpConfig',
     'customuser.apps.CustomuserConfig',
+    ########################## Additional Apps###################################
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
