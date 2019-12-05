e9d3ab562c06a17711dcb2814d51eeefa96ad0dd
Navid Rida <navid.anzum88@gmail.com>
2019-12-05 19:25:24 +0600

    login page

--- rubik/urls.py
+++ rubik/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('erp/', include('erp.urls')),
