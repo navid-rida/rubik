13516ad296a262636c0a7776cbdd669a0aa6b7c6
navid-rida <navid.anzum88@gmail.com>
2020-04-12 20:12:09 +0600

    Product, Quotation, QuotationProduct Model create

--- erp/models.py
+++ erp/models.py
@@ -11,4 +11,33 @@ class Customer(models.Model):
     address = models.TextField('Shipping Address', max_length=300)
     comment = models.CharField(max_length=300)
 
+    def __str__(self):
+        return self.name+" "+self.cell
 
+
+class Product(models.Model):
+    name  = models.CharField('Product Name', max_length=50)
+    paper_material = models.CharField('Paper material', max_length=50)
+    height = models.DecimalField('Height (in inch)', max_digits = 4, decimal_places = 2)
+    width = models.DecimalField('Width (in inch)', max_digits = 4, decimal_places = 2)
+    length = models.DecimalField('Length (in inch)', max_digits = 4, decimal_places = 2)
+    color_number = models.IntegerField('Number of Printer Colors') # Restrict to 12 in validation
+
+    def __str__(self):
+        return self.name
+
+class Quotation(models.Model):
+    product = models.ManyToManyField(Product, through='QuotationProduct')
+    shipping_charge = models.DecimalField('Shipping Charge', max_digits=20, decimal_places=2)
+    vat_percent = models.DecimalField('Vat (Percent)', max_digits=5, decimal_places=2)
+    tax_percent = models.DecimalField('Tax (Percent)', max_digits=5, decimal_places=2)
+    advance = models.DecimalField('Advance Payment', max_digits=20, decimal_places=2)
+    later_payment = models.DecimalField('To be paid later', max_digits=20, decimal_places= 2)
+    customer = models.ForeignKey(Customer, on_delete= models.CASCADE)
+
+
+class QuotationProduct(models.Model):
+    product = models.ForeignKey(Product, on_delete= models.CASCADE)
+    quotation = models.ForeignKey(Quotation, on_delete= models.CASCADE)
+    unit = models.IntegerField('Unit')
+    price = models.DecimalField('Unit Price (BDT)', max_digits=20, decimal_places=2)
