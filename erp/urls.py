5b089a745a2d8429ad794ba834a8c80ff86f2902
navid-rida <navid.anzum88@gmail.com>
2020-04-12 20:14:03 +0600

    Creatview for models Product, Customer, Quotation

--- erp/urls.py
+++ erp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/customer', views.CustomerCreateView.as_view(), name='create_customer'),
+    path('add/product', views.ProductCreateView.as_view(), name='create_product'),
+    path('add/quotation', views.QuotationCreateView.as_view(), name='create_quotation'),
 ]
--- erp/views.py
+++ erp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required,user_passes_test
 from django.views.generic.edit import CreateView
-from .models import Customer
+from .models import Customer, Product, Quotation
 
 
 # Create your views here.
@@ -17,4 +18,19 @@ def index(request):
 class CustomerCreateView(CreateView):
     model=Customer
     template_name_suffix = '_create_form'
-    fields=['name',]
+    fields=['name','email','cell','address','comment']
+    success_url= reverse_lazy('create_customer')
+
+
+class ProductCreateView(CreateView):
+    model=Product
+    template_name_suffix = '_create_form'
+    fields=['name','paper_material','height','width','length','color_number']
+    success_url= reverse_lazy('create_product')
+
+
+class QuotationCreateView(CreateView):
+    model=Quotation
+    template_name = 'erp/quotation/quotation_create_form.html'
+    fields=['product','shipping_charge','vat_percent','tax_percent','advance','later_payment', 'customer']
+    success_url= reverse_lazy('create_quotation')
