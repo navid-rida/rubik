04b0000f12a05b14ee828dbb9af5c3d593273f5f
navid-rida <navid.anzum88@gmail.com>
2020-04-16 00:37:09 +0600

    Registering models

--- erp/admin.py
+++ erp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Customer, Product
+from .models import Customer, Product, Quotation, QuotationProduct
 
 # Register your models here.
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Quotation)
+admin.site.register(QuotationProduct)
