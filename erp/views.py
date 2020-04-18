09a9e4a511d8e8a1e530b4c394ada7f2077e5016
navid-rida <navid.anzum88@gmail.com>
2020-04-18 20:27:17 +0600

    Success message and other template fixes

--- erp/views.py
+++ erp/views.py
@@ -7,6 +7,8 @@ from django.views.generic.edit import CreateView, UpdateView
 from .models import *
 from django.db import transaction
 
+from django.contrib.messages.views import SuccessMessageMixin
+
 from .forms import *
 
 # Create your views here.
@@ -18,26 +20,49 @@ def index(request):
 
 
 #@login_required
-class CustomerCreateView(CreateView):
+class CustomerCreateView(SuccessMessageMixin, CreateView):
     model=Customer
-    template_name_suffix = '_create_form'
+    template_name = 'erp/customer/customer_create_form.html'
     fields=['name','email','cell','address','comment']
+    success_message = "Customer %(name)s was created successfully with ID No: %(id)s "
     success_url= reverse_lazy('create_customer')
 
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            name=self.object.name,
+            id = self.object.id,
+        )
 
-class ProductCreateView(CreateView):
+class ProductCreateView(SuccessMessageMixin, CreateView):
     model=Product
-    template_name_suffix = '_create_form'
+    template_name = 'erp/product/product_create_form.html'
     fields=['name','paper_material','height','width','length','color_number']
     success_url= reverse_lazy('create_product')
+    success_message = "Product %(name)s was created successfully with ID No: %(id)s "
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            name=self.object.name,
+            id = self.object.id,
+        )
+    
 
 
-class QuotationCreateView(CreateView):
+class QuotationCreateView(SuccessMessageMixin, CreateView):
     model=Quotation
     template_name = 'erp/quotation/quotation_create_form.html'
     #fields=['shipping_charge','vat_percent','tax_percent','advance','later_payment', 'customer']
     form_class = QuotationForm
     success_url= reverse_lazy('create_quotation')
+    success_message = "A quotation was created successfully with Serial No: %(id)s "
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            id = self.object.id,
+        )
 
     def get_context_data(self, **kwargs):
         data = super(QuotationCreateView, self).get_context_data(**kwargs)
